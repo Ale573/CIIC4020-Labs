69965ef81ab5233f6826af46c82d63477d9d4570
ICOM STUDENTS <4020c1@guineo.ece.uprm.edu>
2017-08-28 16:44:35 -0400

    Initial commit.

---
+++ src/orderedStructures/Arithmetic.java
@@ -0,0 +1,21 @@
+package orderedStructures;
+
+public class Arithmetic extends Progression {
+	private double commonDifference; 
+	
+	public Arithmetic(double firstValue, double commonDifference) { 
+		super(firstValue); 
+		this.commonDifference = commonDifference; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current + commonDifference; 
+		return current;
+	}
+	
+	public String toString(){
+		return ("Arith(" + (int)firstValue() + "," + (int) commonDifference + ")");
+	}
+
+}
