339400bc905512baf77321de5794af029fb89b61
4020c1 <4020c1@guineo.ece.uprm.edu>
2017-08-28 17:12:10 -0400

    Complete the exercises.

--- src/orderedStructures/Arithmetic.java
+++ src/orderedStructures/Arithmetic.java
@@ -10,12 +10,27 @@ public class Arithmetic extends Progression {
 	
 	@Override
 	public double nextValue() {
+		
+		if(!initValue){
+			throw new IllegalStateException("Invalid initial input");
+		}
 		current = current + commonDifference; 
 		return current;
 	}
 	
 	public String toString(){
 		return ("Arith(" + (int)firstValue() + "," + (int) commonDifference + ")");
+	}
+	
+	@Override
+	public double getTerm(int n) {
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+		
+		double value = this.firstValue(); 
+		for (int i=1; i<n; i++) 
+			value = nextValue(); 
+		return value; 
 	}
 
 }
--- src/orderedStructures/Geometric.java
+++ src/orderedStructures/Geometric.java
@@ -11,12 +11,27 @@ public class Geometric extends Progression {
 	
 	@Override
 	public double nextValue() {
+		
+		if(!initValue){
+			throw new IllegalStateException("Invalid initial input");
+		}
 		current = current * commonFactor; 
 		return current;
 	}
 	
 	public String toString(){
 		return ("Geom(" + (int)firstValue() + "," + (int) commonFactor + ")");
+	}
+	
+	@Override
+	public double getTerm(int n) {
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+		
+		double value = this.firstValue(); 
+		for (int i=1; i<n; i++) 
+			value = nextValue(); 
+		return value; 
 	}
 
 }
